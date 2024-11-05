d4c5d4f64947d637d640338a564652117a68479d
Luan Felipe Antunes <luanfelipe12.antunes@gmail.com>
2024-11-05 20:43:20 -0300

    Alterando para estilos inline no email

--- resources/views/emails/client_created.blade.php
+++ resources/views/emails/client_created.blade.php
@@ -5,7 +5,95 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Novo orçamento gerado</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: 'Plus Jakarta Sans', sans-serif;
+            background-color: #FFFFFF;
+            color: #000000;
+        }
+
+        .container{
+            width: 75%;
+            margin: 40px auto;
+        }
+
+        .header{
+            margin: 40px 0;
+        }
+
+        .header h1{
+            font-size: 23px;
+            font-weight: 400;
+            line-height: 29px;
+            margin-top: 16px;
+        }
+
+        .line-orange{
+            width: 60px;
+            border: 4px solid #FF8A12;
+            margin: 14px 0;
+        }
+
+        span{
+            font-weight: 700;
+        }
+
+        .info{
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 15px;
+        }
+
+        p{
+            width: 40%;
+        }
+
+        .little-letters{
+            font-weight: 400;
+            font-size: 8px;
+            line-height: 10px;
+            margin-top: 14px;
+        }
+
+        .body-email h4{
+            font-size: 14px;
+            line-height: 18px;
+            font-weight: 400;
+        }
+
+        .body-email p{
+            font-size: 23px;
+            font-weight: 700;
+            line-height: 29px;
+            margin-bottom: 16px;
+        }
+
+        .footer{
+            background-color: #0074A8;
+            width: 100vw;
+            height: 140px;
+        }
+
+        .footer img{
+            margin-top: 40px;
+            margin-left: 40px;
+        }
+
+        @media (max-width: 768px) {
+            p{
+                width: 95%;
+            }
+        }
+    </style>
 
 </head>
 
