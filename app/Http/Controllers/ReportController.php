66b18743314ca42a8000b6e98dc6cdfdb8fca173
Luan Felipe Antunes <luanfelipe12.antunes@gmail.com>
2024-11-14 16:21:18 -0300

    deploy 8

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -137,6 +137,7 @@ class ReportController extends Controller
 
     public function index()
     {
+        //deploy 2
         $client_id = request()->query('client_id');
         $user_id = request()->query('user_id');
         $limit = request()->query('limit');
